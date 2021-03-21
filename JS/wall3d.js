11bdbf0279497c2b0d8c21506b19cad6a8111c12
codeblue25 <chois1031@gmail.com>
2021-03-21 14:53:35 +0900

    창 크기 변환 대응
    resize 이벤트를 통해 창 사이즈에 따라 스크롤바 높이 경신

--- JS/wall3d.js
+++ JS/wall3d.js
@@ -1,13 +1,19 @@
 (function(){
     const house = document.querySelector(".house");
-    let maxScroll = document.body.offsetHeight - window.innerHeight; /*body 전체 높이 - 현재 브라우저 창 높이 = 스크롤바 높이*/
+    let maxScroll; /*body 전체 높이 - 현재 브라우저 창 높이 = 스크롤바 높이*/
 
     window.addEventListener("scroll", function(){
         console.log(pageYOffset); /*스크롤 된 양을 수치화*/
         console.log(pageYOffset / maxScroll); /*실제 스크롤 영역*/
         const zMove = pageYOffset / maxScroll * 980 - 490; /*초기 원근감 값인 490vw만큼 뺀다*/
         house.style.transform = "translateZ(" + zMove + "vw)";
-        
     });
+
+    function resizeHandler(){
+        maxScroll = document.body.offsetHeight - window.innerHeight;
+    } 
+
+    window.addEventListener("resize", resizeHandler); /*브라우저 크기가 변할 때마다 maxScroll을 경신시킴*/
+    resizeHandler();
 
 })(); /*전역변수를 회피하기 위해 즉시 실행 함수 사용*/
