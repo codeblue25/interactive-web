b74916636bbd2f7e8cd08a237f81d348e61ebc68
codeblue25 <chois1031@gmail.com>
2021-03-21 15:59:28 +0900

    progress bar 채우기
    스크롤 비율에 따라 채워지도록 변수 선언 후  scroll 이벤트에 추가

--- JS/wall3d.js
+++ JS/wall3d.js
@@ -1,12 +1,16 @@
 (function(){
     const house = document.querySelector(".house");
     let maxScroll; /*body 전체 높이 - 현재 브라우저 창 높이 = 스크롤바 높이*/
+    const bar = document.querySelector(".progress-bar");
 
     window.addEventListener("scroll", function(){
         console.log(pageYOffset); /*스크롤 된 양을 수치화*/
         console.log(pageYOffset / maxScroll); /*실제 스크롤 영역*/
-        const zMove = pageYOffset / maxScroll * 980 - 490; /*초기 원근감 값인 490vw만큼 뺀다*/
+
+        const scrollGauge = pageYOffset / maxScroll;
+        const zMove = scrollGauge * 980 - 490; /*초기 원근감 값인 490vw만큼 뺀다*/
         house.style.transform = "translateZ(" + zMove + "vw)";
+        bar.style.width = scrollGauge * 100 + "%"; /*스크롤 따라 progress bar 채움*/
     });
 
     function resizeHandler(){
