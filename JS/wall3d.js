9978837eb2a29c0c124ec39788d6f72da9b89c87
codeblue25 <chois1031@gmail.com>
2021-03-22 17:05:57 +0900

    마우스 위치에 따라 시점 바꾸기
    마우스 이벤트에 clientX, clientY를 이용해서 마우스 움직임에 따라 stage클래스 회전시키기

--- JS/wall3d.js
+++ JS/wall3d.js
@@ -2,10 +2,12 @@
     const house = document.querySelector(".house");
     let maxScroll; /*body 전체 높이 - 현재 브라우저 창 높이 = 스크롤바 높이*/
     const bar = document.querySelector(".progress-bar");
+    const mousePos = {x: 0, y: 0};
+    const stage = document.querySelector(".stage");
 
     window.addEventListener("scroll", function(){
-        console.log(pageYOffset); /*스크롤 된 양을 수치화*/
-        console.log(pageYOffset / maxScroll); /*실제 스크롤 영역*/
+        // console.log(pageYOffset); /*스크롤 된 양을 수치화*/
+        // console.log(pageYOffset / maxScroll); /*실제 스크롤 영역*/
 
         const scrollGauge = pageYOffset / maxScroll;
         const zMove = scrollGauge * 980 - 490; /*초기 원근감 값인 490vw만큼 뺀다*/
@@ -13,6 +15,14 @@
         bar.style.width = scrollGauge * 100 + "%"; /*스크롤 따라 progress bar 채움*/
     });
 
+    window.addEventListener("mousemove", function(e){
+        mousePos.x = -1 + (e.clientX / window.innerWidth) * 2;
+        mousePos.y = 1 - (e.clientY / window.innerHeight) * 2;
+        /*마우스 위치를 나타내는 기준을 중심으로 옮기기 위한 식*/
+        stage.style.transform = "rotateX(" + (mousePos.y * 5) + "deg) rotateY(" + (mousePos.x * 5) + "deg)";
+    });
+
+    
     function resizeHandler(){
         maxScroll = document.body.offsetHeight - window.innerHeight;
     } 
